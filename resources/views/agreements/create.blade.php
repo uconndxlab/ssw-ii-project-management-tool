ea5d10b34e3955f7a9aaca7d7e7acf4adb29c69a
SamChichester <lnsschi1225@gmail.com>
2026-03-16 20:22:36 -0400

    Added logging field selection in agreement creation.

--- resources/views/agreements/create.blade.php
+++ resources/views/agreements/create.blade.php
@@ -165,6 +165,128 @@
                         <small class="text-muted">List of certification candidates (placeholder)</small>
                     </div>
 
+                    <div class="mb-4">
+                        <h5 class="mb-3">Activity Logging Fields</h5>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[event_hours]"
+                                           id="activity_logging_config_event_hours"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['event_hours']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_event_hours">
+                                        Event Hours
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[prep_hours]"
+                                           id="activity_logging_config_prep_hours"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['prep_hours']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_prep_hours">
+                                        Prep Hours
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[followup_hours]"
+                                           id="activity_logging_config_followup_hours"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['followup_hours']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_followup_hours">
+                                        Follow-up Hours
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[participant_count]"
+                                           id="activity_logging_config_participant_count"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['participant_count']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_participant_count">
+                                        Participants
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[external_attendees]"
+                                           id="activity_logging_config_external_attendees"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['external_attendees']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_external_attendees">
+                                        External Attendees
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[summary]"
+                                           id="activity_logging_config_summary"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['summary']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_summary">
+                                        Summary
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[follow_up]"
+                                           id="activity_logging_config_follow_up"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['follow_up']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_follow_up">
+                                        Follow-Up
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[strengths]"
+                                           id="activity_logging_config_strengths"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['strengths']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_strengths">
+                                        Strengths
+                                    </label>
+                                </div>
+
+                                <div class="form-check mb-2">
+                                    <input class="form-check-input"
+                                           type="checkbox"
+                                           name="activity_logging_config[recommendations]"
+                                           id="activity_logging_config_recommendations"
+                                           value="1"
+                                           {{ !empty($activityLoggingConfig['recommendations']) ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="activity_logging_config_recommendations">
+                                        Recommendations
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <small class="text-muted">
+                            Select which fields should appear when logging activities for this agreement.
+                        </small>
+                    </div>
+
                     <div class="mb-3">
                         <label class="form-label">Assign Users</label>
                         <div class="border rounded p-3" style="max-height: 300px; overflow-y: auto;">
