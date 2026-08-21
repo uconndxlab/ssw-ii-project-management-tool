3e2099ca7dde243a201ce2f7f4639c06eda244a9
WillSho1 <whshocon@gmail.com>
2026-08-21 18:45:55 -0400

    remove temp prod connection

--- config/database.php
+++ config/database.php
@@ -31,20 +31,6 @@ return [
 
     'connections' => [
 
-        // prod connection config
-        'prod_pg' => [
-            'driver' => 'pgsql',
-            'host' => env('PROD_DB_HOST'),
-            'port' => env('PROD_DB_PORT', 5432),
-            'database' => env('PROD_DB_DATABASE'),
-            'username' => env('PROD_DB_USERNAME'),
-            'password' => env('PROD_DB_PASSWORD'),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'search_path' => 'public',
-            'sslmode' => 'require',
-        ],
-
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
