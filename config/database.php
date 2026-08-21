d50d704ff3b23392b5120f629675a22d64aa3915
WillSho1 <whshocon@gmail.com>
2026-08-21 17:12:54 -0400

    migration aid to prod

--- config/database.php
+++ config/database.php
@@ -180,4 +180,18 @@ return [
 
     ],
 
+    // prod connection config
+    'prod_pg' => [
+        'driver' => 'pgsql',
+        'host' => env('PROD_DB_HOST'),
+        'port' => env('PROD_DB_PORT', 5432),
+        'database' => env('PROD_DB_DATABASE'),
+        'username' => env('PROD_DB_USERNAME'),
+        'password' => env('PROD_DB_PASSWORD'),
+        'charset' => 'utf8',
+        'prefix' => '',
+        'search_path' => 'public',
+        'sslmode' => 'require',
+    ],
+
 ];
